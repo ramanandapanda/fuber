8bba52f192cf2ea4447f960d3a54ad721ba32894
Ramananda Panda <ramanandapanda@hotmail.com>
2021-03-21 12:43:29 +0530

    intial data elements

---
+++ src/data.ts
@@ -0,0 +1,84 @@
+export const data = {
+    cabs: [{
+        "id": "1",
+        "loc": {
+            "lat": "20.951666",
+            "lon": "85.098526"
+        },
+        "color": "red",
+    }, {
+        "id": "2",
+        "loc": {
+            "lat": "28.704060",
+            "lon": "77.102493"
+        },
+        "color": "green",
+    }, {
+        "id": "2",
+        "loc": {
+            "lat": "22.572645",
+            "lon": "85.098526"
+        },
+        "color": "blue",
+    }, {
+        "id": "3",
+        "loc": {
+            "lat": "21.951666",
+            "lon": "89.363892"
+        },
+        "color": "pink",
+    }, {
+        "id": "4",
+        "loc": {
+            "lat": "10.951666",
+            "lon": "55.098526"
+        },
+        "color": "white",
+    }, {
+        "id": "5",
+        "loc": {
+            "lat": "25.951666",
+            "lon": "65.098526"
+        },
+        "color": "cyan",
+    }],
+    customers: [{
+        "id": "1",
+        "name": "Chandler",
+    }, {
+        "id": "2",
+        "name": "Phoebe",
+    }, {
+        "id": "3",
+        "name": "Raechal",
+    }, {
+        "id": "4",
+        "name": "Joey",
+    }, {
+        "id": "1",
+        "name": "Monika",
+    }, {
+        "id": "1",
+        "name": "Ross",
+    },],
+    rides: [
+        {
+            "id": "1",
+            "start_loc": {
+                "lat": "100",
+                "lon": "20"
+            },
+            "end_loc": {
+                "lat": "100",
+                "lon": "20"
+            },
+            "current_loc": {
+                "lat": "100",
+                "lon": "20"
+            },
+            "status": 0,
+            "customer_id": "1",
+            "cab_id": "1"
+        },
+    ]
+}
